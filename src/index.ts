8dd10d5b0724e605a331aa648bc9f58731f84c2d
Juliana Quaresma <julianaqribeiro@hotmail.com>
2026-05-16 21:14:42 -0300

    retirada do visualizador pdf

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@
 import '@pipelinesolucoes/theme';
 
 export { default as ModalMotion } from "./components/ModalMotion";
-export { VisualizadorPdfBase } from "./components/VisualizadorPdfBase";
-export type { VisualizadorPdfBaseProps } from "./components/VisualizadorPdfBase";
 
 export type { BorderProps } from "./types/style/BorderProps";
 export type { LayoutProps } from "./types/style/LayoutProps";
